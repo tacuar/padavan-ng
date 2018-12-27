20b41dd44e85a3602ae0203a29115e1daf41bbcf
Maxim Anisimov <maxim.anisimov.ua@gmail.com>
2018-12-27 13:34:03 +0000

    wpa_supplicant: update to 2.6
    Thx to Developer_MZRIP

--- trunk/user/wpa_supplicant/wpa_supplicant/wpa_passphrase.c
+++ trunk/user/wpa_supplicant/wpa_supplicant/wpa_passphrase.c
@@ -2,22 +2,14 @@
  * WPA Supplicant - ASCII passphrase to WPA PSK tool
  * Copyright (c) 2003-2005, Jouni Malinen <j@w1.fi>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * Alternatively, this software may be distributed under the terms of BSD
- * license.
- *
- * See README and COPYING for more details.
+ * This software may be distributed under the terms of the BSD license.
+ * See README for more details.
  */
 
 #include "includes.h"
 
-#ifndef CONFIG_NO_PBKDF2
-
 #include "common.h"
-#include "sha1.h"
+#include "crypto/sha1.h"
 
 
 int main(int argc, char *argv[])
@@ -60,7 +52,7 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	pbkdf2_sha1(passphrase, ssid, os_strlen(ssid), 4096, psk, 32);
+	pbkdf2_sha1(passphrase, (u8 *) ssid, os_strlen(ssid), 4096, psk, 32);
 
 	printf("network={\n");
 	printf("\tssid=\"%s\"\n", ssid);
@@ -73,11 +65,3 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
-
-#else /* CONFIG_NO_PBKDF2 */
-int main(int argc, char *argv[])
-{
-	printf("CONFIG_NO_PBKDF2 defined - wpa_passphrase disabled\n");
-	return -1;
-}
-#endif /* CONFIG_NO_PBKDF2 */
